d33dc7405a6b54334d4b3b60317d997cd6245e7e
jayc595 <jay.crocker@hotmail.co.uk>
2024-07-07 14:53:35 +0100

    Adding create new bot drawer

--- src/app/(dashboard)/_components/bot-switcher.tsx
+++ src/app/(dashboard)/_components/bot-switcher.tsx
@@ -1,6 +1,9 @@
 import { Button } from '@/components/ui/button'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
+import { Input } from '@/components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Separator } from '@/components/ui/separator'
+import { Textarea } from '@/components/ui/textarea'
 import { cn } from '@/lib/utils'
 import React from 'react'
 
@@ -49,15 +52,39 @@ const BotSwitcher = ({
             </SelectItem>
             ))}
         <Separator/>
-        <SelectItem value="new-bot">
-                <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
-                    <svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="10" y="4" width="4" height="16" fill="black"/>
-                        <rect x="4" y="10" width="16" height="4" fill="black"/>
-                    </svg>
-                    Create new Bot
-                </div>
-            </SelectItem>
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button className='w-full' variant='select' value="new-bot">
+                  <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
+                      <svg width="100" height="100" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <rect x="10" y="4" width="4" height="16" fill="black"/>
+                          <rect x="4" y="10" width="16" height="4" fill="black"/>
+                      </svg>
+                      Create new Bot
+                  </div>
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <DrawerTitle>Create New Bot</DrawerTitle>
+                <DrawerDescription>Shape your Bot's identity</DrawerDescription>
+              </DrawerHeader>
+              <Select>
+                
+              </Select>
+              <Input placeholder="Label your bot"/>
+              <Input placeholder="Name your bot"/>
+              <Textarea></Textarea>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+          </DrawerFooter>
+            </div>
+          </DrawerContent>
+        </Drawer>    
       </SelectContent>
     </Select>
   )
